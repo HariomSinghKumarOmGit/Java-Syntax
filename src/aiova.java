024b881291a11cbe457741d97e74556bad55b6ec
hariom singh <hariomsingh@Mac.lan>
2025-11-14 19:05:12 +0530

    First commit, learn some stuff

---
+++ src/aiova.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+
+
+public class aiova {
+    public static void main(String[] args) {
+
+        // non - primitive type
+        // concatenate
+/*
+        String name1 ="Ajay";
+        String name2 ="Aarav";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name1.charAt(3));
+        String name5 = name1.replace('A', 'V');
+        System.out.println(name5);
+        System.out.println(name3.substring(5, 11)); */
+
+
+        // Arrays
+        /*
+        int age= 30;
+        int[] marks = new int[3];
+            marks[0]=110;
+            marks[1]=12;
+            marks[2]=30;
+
+        System.out.println(marks.length);
+
+        // short array
+        System.out.println(marks[0]);
+        Arrays.sort(marks);
+        System.out.println(marks[0]);
+*/
+//        1d array
+/*
+        int[] marks = {98, 27, 30};
+
+        // 2d array
+        int [][] finalmarks = {{89, 98, 69}, {97, 73, 25}};
+        System.out.println(finalmarks[0][2]);
+
+ */
+        // casting
+        // capacity of holding values like double has more capacity compare to int
+
+
+    }
+}
